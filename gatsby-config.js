67468803f48d9ad5dc850598550b547087c71e23
Sid Ahmed <brahimi.sdhmd@gmail.com>
2022-02-24 07:38:37 +0100

    feat: add gtm

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,5 +44,30 @@ module.exports = {
     "gatsby-plugin-postcss",
     `gatsby-plugin-advanced-sitemap`,
     `gatsby-plugin-robots-txt`,
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-5N9J3MM",
+
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+
+        // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        dataLayerName: "sn-dataLayer",
+
+        // Defaults to false
+        enableWebVitalsTracking: true,
+      },
+    },
   ],
 }
