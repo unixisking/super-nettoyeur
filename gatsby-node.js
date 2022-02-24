345da9c3e8e2c291c0a50762ce58185142109757
Sid Ahmed <brahimi.sdhmd@gmail.com>
2022-02-24 07:26:05 +0100

    fix: storyblok problems

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,55 +4,60 @@
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
 
- const path = require('path')
+const path = require("path")
 
- exports.createPages = ({ graphql, actions }) => {
-    const { createPage } = actions
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
 
-    return new Promise((resolve, reject) => {
-        // sets the template for the pages
-        const storyblokEntry = path.resolve('src/templates/page.js')
-    
-        // gets all storyblok stories with the content type 'page'
-        resolve(
-          graphql(
-            `{
-              stories: allStoryblokEntry(filter: {field_component: {eq: "page"}}) {
-                edges {
-                  node {
-                    id
-                    name
-                    slug
-                    field_component
-                    full_slug
-                    content
-                  }
+  return new Promise((resolve, reject) => {
+    // sets the template for the pages
+    const storyblokEntry = path.resolve("src/templates/page.js")
+
+    // gets all storyblok stories with the content type 'page'
+    resolve(
+      graphql(
+        `
+          {
+            stories: allStoryblokEntry(
+              filter: { field_component: { eq: "page" } }
+            ) {
+              edges {
+                node {
+                  id
+                  name
+                  slug
+                  field_component
+                  full_slug
+                  content
                 }
               }
-            }`
-          ).then(result => {
-            if (result.errors) {
-              console.log(result.errors)
-              reject(result.errors)
             }
-    
-            const entries = result.data.stories.edges
-            
-            // creates a page for each entry with the storyblok slug
-            entries.forEach((entry) => {
-                // skip home story
-                if(entry.slug !== "home") {
-                    const page = {
-                        path: `/${entry.node.full_slug}`,
-                        component: storyblokEntry,
-                        context: {
-                            story: entry.node
-                        }
-                    }
-                    createPage(page)
-                }
-            })
-          })
-        )
+          }
+        `
+      ).then(result => {
+        if (result.errors) {
+          console.log(result.errors)
+          reject(result.errors)
+        }
+
+        const entries = result.data.stories.edges
+
+        // creates a page for each entry with the storyblok slug
+        entries.forEach(entry => {
+          // skip home story
+          console.log("pageslug", entry)
+          if (entry.slug !== "home" && entry.node.name !== "layout") {
+            const page = {
+              path: `/${entry.node.full_slug}`,
+              component: storyblokEntry,
+              context: {
+                story: entry.node,
+              },
+            }
+            createPage(page)
+          }
+        })
       })
- }
+    )
+  })
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,27 +1,12 @@
 import * as React from "react"
-import { graphql, useStaticQuery } from "gatsby"
 import PropTypes from "prop-types"
-
-import DynamicComponent from "./dynamicComponent"
 
 import "./layout.css"
 
 const Layout = ({ children, location }) => {
-  const data = useStaticQuery(graphql`
-    query LayoutQuery {
-      storyblokEntry(path: { eq: "global" }) {
-        uuid
-        content
-      }
-    }
-  `)
-  let story = JSON.parse(data.storyblokEntry.content)
-  // story = useStoryblok(story, location)
-  const footer = story.body.find(blok => blok.component === "footer")
   return (
     <div style={{ background: "#F0F8ED" }}>
       <main>{children}</main>
-      <DynamicComponent blok={footer} key={footer._uid} />
     </div>
   )
 }
