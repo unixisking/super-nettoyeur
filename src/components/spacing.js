0cf2ba7a06e7a18a5111e8209d1cbf6a9468847a
Sid Ahmed <brahimi.sdhmd@gmail.com>
2022-03-14 09:53:48 +0100

    fix: spacing component issue

--- src/components/spacing.js
+++ src/components/spacing.js
@@ -1,5 +1,5 @@
 import React from "react"
 
 export default function Spacing({ blok: { top } }) {
-  return <div className={`mt-${top}`} />
+  return <div className={`hidden lg:block mt-${top}`} />
 }
